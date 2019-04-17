355f34542af974d50f3b16c037f22322a581a108
Eli Saracino <esaracin@bu.edu>
2019-04-17 10:52:33 -0400

    first output .avi added

--- create_skeleton/PoseAnalysis.py
+++ create_skeleton/PoseAnalysis.py
@@ -5,17 +5,17 @@ import numpy as np
 MODE = "MPI" # Keypoint Detection Dataset
 
 if MODE is "COCO":
-    protoFile = "pose/coco/pose_deploy_linevec.prototxt"
-    weightsFile = "pose/coco/pose_iter_440000.caffemodel"
+    protoFile = "pose_models/pose/coco/pose_deploy_linevec.prototxt"
+    weightsFile = "pose_models/pose/coco/pose_iter_440000.caffemodel"
     nPoints = 18
     POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
                   [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
 
 elif MODE is "MPI":
     # specifies the architecture of the neural network – how the different layers are arranged etc.
-    protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+    protoFile = "pose_models/pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
     # stores the weights of the trained model, trained on MPII dataset
-    weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
+    weightsFile = "pose_models/pose/mpi/pose_iter_160000.caffemodel"
     nPoints = 15
     POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14], [14, 8], [8, 9], [9, 10], [14, 11],
                   [11, 12], [12, 13]]
